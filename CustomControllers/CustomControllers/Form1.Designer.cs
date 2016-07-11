5005fe3da9163e23e98b1601d61ee2a5d99df1e2
abeyrathne8887 <abeyrathne8887@gmail.com>
2016-07-11 16:29:55 +0530

    Add new One

--- CustomControllers/CustomControllers/Form1.Designer.cs
+++ CustomControllers/CustomControllers/Form1.Designer.cs
@@ -28,21 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
             this.toggleButton1 = new CustomControllers.ToggleButton();
+            this.toggleButton2 = new CustomControllers.ToggleButton();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(240, 227);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 45);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // toggleButton1
             // 
@@ -52,21 +40,38 @@
             this.toggleButton1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.toggleButton1.Image = global::CustomControllers.Properties.Resources._1468252351_device_volume_loudspeaker_speaker_up_glyph;
             this.toggleButton1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.toggleButton1.Location = new System.Drawing.Point(54, 109);
+            this.toggleButton1.Location = new System.Drawing.Point(54, 105);
             this.toggleButton1.Name = "toggleButton1";
-            this.toggleButton1.Size = new System.Drawing.Size(63, 50);
+            this.toggleButton1.Size = new System.Drawing.Size(58, 54);
             this.toggleButton1.TabIndex = 0;
             this.toggleButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.toggleButton1.ToggleOffColor = System.Drawing.SystemColors.Control;
             this.toggleButton1.ToggleOnColor = System.Drawing.SystemColors.Highlight;
             this.toggleButton1.UseVisualStyleBackColor = false;
             // 
+            // toggleButton2
+            // 
+            this.toggleButton2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.toggleButton2.DisplayText = "";
+            this.toggleButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.toggleButton2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.toggleButton2.Image = global::CustomControllers.Properties.Resources._1468252351_device_volume_loudspeaker_speaker_up_glyph;
+            this.toggleButton2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.toggleButton2.Location = new System.Drawing.Point(118, 105);
+            this.toggleButton2.Name = "toggleButton2";
+            this.toggleButton2.Size = new System.Drawing.Size(58, 54);
+            this.toggleButton2.TabIndex = 1;
+            this.toggleButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.toggleButton2.ToggleOffColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.toggleButton2.ToggleOnColor = System.Drawing.Color.Red;
+            this.toggleButton2.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(500, 395);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.toggleButton2);
             this.Controls.Add(this.toggleButton1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -77,7 +82,7 @@
         #endregion
 
         private ToggleButton toggleButton1;
-        private System.Windows.Forms.Button button1;
+        private ToggleButton toggleButton2;
     }
 }
 
